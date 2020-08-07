f69204a991221ac64973b41d0a469c745ded9f26
root <root@ip-172-31-43-81.ap-south-1.compute.internal>
2020-08-07 07:41:22 +0000

    Added basic indexing of all the pages and searching feature

--- cmd/mini-google/main.go
+++ cmd/mini-google/main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/amanchourasiya/mini-google/pkg/engine"
 )
 
 func main() {
-	docs, err := engine.LoadDocuments("/root/enwiki-latest-abstract1.xml")
-	if err != nil {
-		fmt.Printf("Error loading xml document %s", err)
-	}
-	_ = engine.Search(docs, "cat")
-	fmt.Printf("Results found")
+	idx := engine.CreateIndex()
+	idx.Search("Small WIld cat")
 }
